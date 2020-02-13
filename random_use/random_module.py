d7a95455843822ec3f771559d2f7d6b25a0b0b29
shterneregen <shterneregen@gmail.com>
2020-02-13 22:20:29 +0300

    list comprehension

--- random_use/random_module.py
+++ random_use/random_module.py
@@ -9,3 +9,9 @@ for i in range(3):
 members = ['John', 'Mary', 'Bob', 'Mosh']
 leader = random.choice(members)
 print(leader)
+
+ages = [random.randint(0, 100) for _ in range(20)]
+print(ages)
+
+squares = [x ** 2 for x in range(10)]
+print(squares)
